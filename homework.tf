e0732dce63f4a05913cc514852790d3836dd7634
Azizjon-sys <58641380+Azizjon-sys@users.noreply.github.com>
2023-01-02 12:55:36 -0500

    Add files via upload

---
+++ homework.tf
@@ -0,0 +1,37 @@
+resource "aws_iam_group" "developers0" {
+  name = "admin_group"
+}
+
+resource "aws_iam_group" "developers1" {
+  name = "data_group"
+}
+
+resource "aws_iam_group" "developers" {
+  name = "ops_group1"
+}
+
+resource "aws_iam_group" "developers3" {
+  name = "server_group"
+}
+
+resource "aws_iam_group" "developers4" {
+  name = "monitoring_group"
+}
+
+
+resource "aws_iam_group" "developers5" {
+  name = "finance_group"
+}
+
+resource "aws_iam_group" "developers6" {
+  name = "dev_group"
+}
+
+resource "aws_iam_group" "developers7" {
+  name = "test_group"
+}
+
+
+resource "aws_iam_group" "developers8" {
+  name = "prod_group"
+}
